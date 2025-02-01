5639e26fb833cbc2574f69a3d84eb8fbffdfc80d
inkweon7269 <inkweon7269@gmail.com>
2025-02-01 17:15:04 +0900

    NavLink 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
 const Index = () => {
-    return <div>Index</div>;
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to='/' end>
+                        Index
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/home' end>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about' end>
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about/10'>About Detail</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Index;
